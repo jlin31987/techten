c5e58717fe340017284aab78963f27022d18b54d
kimcarrington <165577489+kimcarrington@users.noreply.github.com>
2024-10-14 08:34:19 -0500

    Update passwordGenerator.test.js

--- __test__/passwordGenerator.test.js
+++ __test__/passwordGenerator.test.js
@@ -12,8 +12,18 @@ describe('Password Generator', () => {
         expect(/[A-Z]/.test(password)).toBe(true);
     });
 
+    test('should include lowercase letters when selected', () => {
+        const password = generatePassword(10, 'false', 'true', 'false', 'false');
+        expect(/a-z/].test(password)).toBe(true);
+    });
+
     test('should include numbers when selected', () => {
         const password = generatePassword(10, 'false', 'false', 'true', 'false');
         expect(/[0-9]/.test(password)).toBe(true);
+    });
+
+    test('should include special characters when selected', () => {
+        const password = generatePassword(10, 'false', 'false', 'false', 'true');
+        expect(/[!@#$%^&*()-_=+[]{}<>?]/).test(password)).toBe(true);
     });
 });
