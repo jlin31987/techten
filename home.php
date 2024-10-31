569097831d39abcc758958c16c3981155eafe310
Kennypham777 <skypham1234@gmail.com>
2024-10-31 18:31:31 -0400

    Creates home.php with a baseline html

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="header">
+        Password Generator
+    </header>
+
+    <div class="content-container">
+        <div class="password-generator">
+            <input type="text" id="generatedPassword" placeholder="Generated Password">
+            <button id="generateBtn">Generate</button>
+        </div>
+    </div>
+</body>
+</html>
